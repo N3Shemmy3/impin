b8eadc9c6b2841885c6fbcb83584aabb320ba212
Shemmy <shemmynyirenda3@gmail.com>
2023-06-24 23:37:37 +0200

    added a switch

--- pages/video/[id].vue
+++ pages/video/[id].vue
@@ -39,26 +39,25 @@
 
 	/* The slider */
 	.slider {
+		@apply bg-colorBackgroundLight;
 		position: absolute;
 		cursor: pointer;
 		top: 0;
 		left: 0;
 		right: 0;
 		bottom: 0;
-		background-color: #ccc;
 		-webkit-transition: 0.4s;
 		transition: 0.4s;
 	}
 
 	.slider:before {
-		@apply bg-colorPrimaryLight;
+		@apply bg-colorBackgroundLight;
 		position: absolute;
 		content: "";
 		height: 26px;
 		width: 26px;
 		left: 4px;
 		bottom: 4px;
-		background-color: white;
 		-webkit-transition: 0.4s;
 		transition: 0.4s;
 	}
